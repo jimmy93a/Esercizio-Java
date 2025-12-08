0a3d0385706c1fd5ab5245649c93721c7c5450cf
Jimmy Albano <jimmy93a@gmail.com>
2025-12-08 13:48:06 +0100

    Inizio esercizio java

---
+++ main.js
@@ -0,0 +1,25 @@
+let num1 = 1;
+let num2 = 2;
+
+console.log(num1);
+console.log(num2);
+
+let stringa = "Sono una stringa";
+
+console.log(stringa);
+
+stringa = "Sono una copia della stringa";
+
+console.log(stringa);
+
+const PIGRECO = 3.14159265359;
+
+console.log(PIGRECO);
+
+PIGRECO = 22;
+
+console.log(PIGRECO);
+
+
+
+
